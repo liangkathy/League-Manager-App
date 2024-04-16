9fd40684479ecec4dbf0ec582c664e9b3c3a9b87
rpearson <rpearson@indeed.com>
2024-04-16 15:07:57 -0700

    Basic Bones

---
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/LeagueManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaguemanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueManagerAppApplication.class, args);
+    }
+
+}
---
+++ LeagueManagerApp/src/test/java/com/example/leaguemanagerapp/LeagueManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaguemanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
