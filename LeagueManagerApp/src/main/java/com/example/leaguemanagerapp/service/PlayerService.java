f5dd828d902ee6b64f063e28dfde9899849f007a
rpearson <rpearson@indeed.com>
2024-04-16 17:46:38 -0700

    created feature player service

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/service/PlayerService.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/service/PlayerService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 public class PlayerService {
     @Autowired
     IPlayerRepository playerRepository;
-    @Autowired
-    ITeamRepository teamRepository; //for later
+ 
     
 }
