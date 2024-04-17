3bb6b2127f6bc24dd94d3509c0529868a6179420
rpearson <rpearson@indeed.com>
2024-04-16 17:31:50 -0700

    created player service

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/service/PlayerService.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/service/PlayerService.java
@@ -1,4 +1,15 @@
 package com.example.leaguemanagerapp.service;
 
+import com.example.leaguemanagerapp.repository.IPlayerRepository;
+import com.example.leaguemanagerapp.repository.ITeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlayerService {
+    @Autowired
+    IPlayerRepository playerRepository;
+    @Autowired
+    ITeamRepository teamRepository; //for later
+    
 }
