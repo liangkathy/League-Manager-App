27cd368f7b15747185a302576e70cf2da1b54f9d
rpearson <rpearson@indeed.com>
2024-04-17 09:25:08 -0700

    updating player Repo package files

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/repository/IPlayerRepository.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/repository/IPlayerRepository.java
@@ -1,2 +1,7 @@
-public interface IPlayerRepository {
+package com.example.leaguemanagerapp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPlayerRepository extends JpaRepository {
 }
