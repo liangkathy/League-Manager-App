1aa21e37c1d343bb33d54f5af1598454368f2819
rpearson <rpearson@indeed.com>
2024-04-16 17:29:10 -0700

    created player repo

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/repository/IPlayerRepository.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/repository/IPlayerRepository.java
@@ -1,4 +1,8 @@
 package com.example.leaguemanagerapp.repository;
 
-public interface IPlayerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPlayerRepository extends JpaRepository {
 }
