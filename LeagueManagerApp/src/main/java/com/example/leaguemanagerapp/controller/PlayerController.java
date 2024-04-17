25edb0d9ddb65cb5e111c9e8848db15d4543f720
rpearson <rpearson@indeed.com>
2024-04-17 10:21:15 -0700

    first crud: retrieve player by ID

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/controller/PlayerController.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/controller/PlayerController.java
@@ -1,18 +1,26 @@
 package com.example.leaguemanagerapp.controller;
 
+import com.example.leaguemanagerapp.model.Player;
 import com.example.leaguemanagerapp.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PlayerController {
     @Autowired
     PlayerService playerService;
-    //Crud operations below
 
     //BASIC CRUD
 
-    //GET
+    //GET - Retrieve a player by ID
+@GetMapping("/players/{id}")
+    public List<Player> getPlayerbyID(@PathVariable int id) {
+    return playerService.getPlayerbyID(id); }
+}
 
     //POST
 
