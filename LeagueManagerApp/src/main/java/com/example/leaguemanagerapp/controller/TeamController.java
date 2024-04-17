ff401311937d20df73a3d195afbef5065db4b66a
liangkathy <jliang@indeed.com>
2024-04-16 19:00:14 -0500

    created team controller

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/controller/TeamController.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/controller/TeamController.java
@@ -1,4 +1,12 @@
 package com.example.leaguemanagerapp.controller;
 
+import com.example.leaguemanagerapp.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TeamController {
+
+    @Autowired
+    TeamService teamService;
 }
