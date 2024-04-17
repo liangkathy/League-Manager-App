39dac51ad516561348d994e4ad3519762cd2cb66
rpearson <rpearson@indeed.com>
2024-04-16 17:27:11 -0700

    created player model

--- LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/model/Player.java
+++ LeagueManagerApp/src/main/java/com/example/leaguemanagerapp/model/Player.java
@@ -1,4 +1,25 @@
 package com.example.leaguemanagerapp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+
+
+    @JsonIgnore
+    @ManyToOne //(cascade = CascadeType.PERSIST) - this might be able to be excluded
+    @JoinColumn (name = "team_id", nullable =false)
+    private Team team;
+
 }
